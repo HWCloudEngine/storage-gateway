73a18b6cf3205f2f9cd9cb6e94c71ef8a966e6fc
smile-luobin <luobin_smile@163.com>
2016-07-30 03:31:26 +0000

    Implement replayer rpc client
    Change-Id: I54fe2ce7f91b0c0d5cfcb9a907c9097fc028e543

---
+++ src/rpc/clients/replayer_client.cpp
@@ -0,0 +1,75 @@
+/*
+ * replayer_client.cpp
+ *
+ *  Created on: 2016Äê7ÔÂ29ÈÕ
+ *      Author: smile-luobin
+ */
+
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/lexical_cast.hpp>
+#include "replayer_client.hpp"
+
+ReplayerClient::ReplayerClient(std::shared_ptr<Channel> channel) :
+        stub_(Consumer::NewStub(channel)),
+        lexical_cast<string>(uuid(boost::uuids::random_generator()())),
+        consumer_type(0) {
+}
+
+bool ReplayerClient::GetJournalMarker(const std::string& vol_id,
+        JournalMarker& marker_) {
+    GetJournalMarkerRequest request;
+    request.set_uuid(uuid);
+    request.set_vol_id(vol_id);
+
+    GetJournalMarkerResponse reply;
+    ClientContext context;
+
+    Status status = stub_->GetJounralMarker(&context, request, &reply);
+    if (status.ok() && (result == DRS_OK)) {
+        marker_ = reply.marker;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool ReplayerClient::GetJournalList(const std::string& vol_id,
+        const JournalMarker& marker, int limit,
+        std::list<std::string>& journal_list_) {
+    GetJournalListRequest request;
+    request.set_limit(limit);
+    request.set_vol_id(vol_id);
+    request.set_allocated_marker(marker);
+
+    GetJournalListResponse reply;
+    ClientContext context;
+
+    Status status = stub_->GetJournalList(&context, request, &reply);
+    if (status.ok() && (result == DRS_OK)) {
+        for (int i = 0; i < reply.journals_size(); i++) {
+            journal_list_.push_back(reply.journals(i));
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool ReplayerClient::UpdateConsumerMarker(const JournalMarker& marker, const std::string& vol_id) {
+    UpdateConsumerMarkerRequest request;
+    request.set_uuid(uuid);
+    request.set_vol_id(vol_id);
+    request.set_type(consumer_type);
+    request.set_allocated_marker(marker);
+
+    UpdateConsumerMarkerResponse reply;
+    ClientContext context;
+
+    Status status = stub_->UpdateConsumerMarker(&context, request, &reply);
+    if (status.ok() && (result == DRS_OK)){
+        return true;
+    }else{
+        return false;
+    }
+}
---
+++ src/rpc/clients/replayer_client.hpp
@@ -0,0 +1,40 @@
+/*
+ * replayer_client.hpp
+ *
+ *  Created on: 2016Äê7ÔÂ29ÈÕ
+ *      Author: smile-luobin
+ */
+
+#ifndef RPC_REPLAYER_CLIENT_HPP_
+#define RPC_REPLAYER_CLIENT_HPP_
+
+#include <grpc++/grpc++.h>
+#include "../consumer.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using huawei::proto::Consumer;
+using huawei::proto::GetJournalListRequest;
+using huawei::proto::GetJournalListResponse;
+using huawei::proto::GetJournalMarkerRequest;
+using huawei::proto::GetJournalMarkerResponse;
+using huawei::proto::UpdateConsumerMarkerRequest;
+using huawei::proto::UpdateConsumerMarkerResponse;
+using huawei::proto::RESULT;
+using huawei::proto::CONSUMER_TYPE;
+
+class ReplayerClient {
+public:
+    ReplayerClient(std::shared_ptr<Channel> channel);
+    bool GetJournalMarker(const std::string& vol_id, JournalMarker& marker_);
+    bool GetJournalList(const std::string& vol_id, const JournalMarker& marker,
+            int limit, std::list<std::string>& journal_list_);
+    bool UpdateConsumerMarker(const JournalMarker& marker, const std::string& vol_id);
+private:
+    std::unique_ptr<Writer::Stub> stub_;
+    CONSUMER_TYPE consumer_type;
+    std::string uuid;
+};
+
+#endif /* RPC_REPLAYER_CLIENT_HPP_ */
