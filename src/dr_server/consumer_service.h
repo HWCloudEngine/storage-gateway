e1e3b2c93eb006cee7ad70e22495985377ccdd9c
toddyLee <layu_lee@yahoo.com>
2016-08-01 15:15:02 +0800

    init journal meta service
    1. init journal meta service interface
    2. rename journal_meta_service to journal_meta_manager
    3. use boost log instead of std::cout and other changes on review
    
    Change-Id: If78333238bc91887b9737326cd4c1b90cc6fe7c7

--- src/dr_server/consumer_service.h
+++ src/dr_server/consumer_service.h
@@ -12,14 +12,28 @@
 #define CONSUMER_SERVICE_H_
 #include <grpc++/grpc++.h>
 #include "rpc/consumer.grpc.pb.h"
+#include "journal_meta_manager.h"
 using grpc::Status;
 using grpc::ServerContext;
 using huawei::proto::Consumer;
 using huawei::proto::GetJournalMarkerRequest;
 using huawei::proto::GetJournalMarkerResponse;
+using huawei::proto::GetJournalListRequest;
+using huawei::proto::GetJournalListResponse;
+using huawei::proto::UpdateConsumerMarkerRequest;
+using huawei::proto::UpdateConsumerMarkerResponse;
 
-class ReplayerServiceImpl final : public Consumer::Service {
+class ConsumerServiceImpl final : public Consumer::Service {
+private:
+    JournalMetaManager *_meta;
+public:
+    ConsumerServiceImpl(JournalMetaManager *meta);
     Status GetJournalMarker(ServerContext* context, const GetJournalMarkerRequest* request,
             GetJournalMarkerResponse* reply) override;
+    Status GetJournalList(ServerContext* context, const GetJournalListRequest* request,
+            GetJournalListResponse* response) override;
+    // update consumer maker when time out or comsumed a batch of logs
+    Status UpdateConsumerMarker(ServerContext* context,
+            const UpdateConsumerMarkerRequest* request, UpdateConsumerMarkerResponse* response) override;
 };
 #endif
