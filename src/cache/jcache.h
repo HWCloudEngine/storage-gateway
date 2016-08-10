992b730d159ca2ce4d9362020eda7498364f6cd0
dsnlevi <hnlgwujing@sina.com>
2016-08-09 22:16:27 -0400

    Implement cache module
    rename proxy to more clean as cache_proxy
    
    Change-Id: I059c4e5e21a9f991872d85ad4d8d1d51aa2e5c04

---
+++ src/cache/jcache.h
@@ -0,0 +1,68 @@
+#ifndef _JCACHE_H
+#define _JCACHE_H 
+#include <iostream>
+#include <string.h>
+#include <map>
+#include "common.h"
+using namespace std;
+
+class Jkey
+{
+public: 
+    Jkey(uint64_t seq):m_seq(seq){}
+    Jkey(const Jkey& other):m_seq(other.m_seq){}
+    Jkey(Jkey&& other):m_seq(std::move(other.m_seq)){}
+
+    Jkey& operator=(const Jkey& other){
+        if(this != &other)
+            this->m_seq = other.m_seq;
+        return *this;
+    }
+    
+    friend bool operator<(const Jkey&a, const Jkey& b);
+    friend ostream& operator<<(ostream& cout, const Jkey& key);
+    uint64_t m_seq; //log sequence no
+};
+
+struct JkeyCompare{
+    bool operator()(const Jkey& a, const Jkey& b){
+        return a < b;
+    }
+};
+typedef map<Jkey, shared_ptr<CEntry>, JkeyCompare> jcache_map_t;
+typedef map<Jkey, shared_ptr<CEntry>, JkeyCompare>::iterator jcache_itor_t;
+
+class Jcache {
+public:
+    Jcache(){}
+    Jcache(string blk_dev):m_blkdev(blk_dev){
+        m_jcache.clear(); 
+    }
+
+    Jcache(const Jcache& other) = delete;
+    Jcache(Jcache&& other) = delete;
+    Jcache& operator=(const Jcache& other) = delete;
+    Jcache& operator=(Jcache&& other) = delete;
+    
+    ~Jcache(){
+        m_jcache.clear(); 
+    }
+    
+    bool add(Jkey key, shared_ptr<CEntry> value);
+    shared_ptr<CEntry> get(Jkey key);
+    bool update(Jkey key, shared_ptr<CEntry> value);
+    bool del(Jkey key);
+    
+    /*get the minimum Jkey in jcache*/
+    Jkey top();
+
+    /*debug*/
+    void trace();
+
+private:
+    string                        m_blkdev;  //original block device
+    Mutex                         m_mutex;    //read write lock
+    jcache_map_t                  m_jcache;  //act as queue
+};
+
+#endif
---
+++ src/cache/main.cpp
@@ -0,0 +1,103 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <memory>
+#include "../common/log_header.h"
+#include "../journal/message.hpp"
+#include "cache_proxy.h"
+
+using namespace std;
+using namespace Journal;
+
+struct log_header_deleter
+{
+    void operator()(log_header_t* p){
+        cout << "log header deleter called " << endl;
+        free(p);
+    }
+};
+
+int main(int argc, char** argv)
+{
+    string blk = "./blk";
+    CacheProxy proxy(blk);
+
+    string   log_file = "./log";
+
+    uint64_t  log_seq  = 0;
+    off_t     log_off  = 0;
+    off_len_t i1  = {10, 10};
+    size_t lsize  = sizeof(log_header_t) + sizeof(off_len_t) + i1.length;
+    log_header_t* log_1 = (log_header_t*)malloc(lsize);
+    log_1->count = 1;
+    memcpy((char*)log_1->off_len, &i1, sizeof(off_len_t));
+    char* d1 = (char*)malloc(i1.length);
+    memcpy((char*)log_1+sizeof(log_header_t)+sizeof(off_len_t), d1, i1.length);
+    //shared_ptr<log_header_t> le1(log_1, log_header_deleter());
+    shared_ptr<ReplayEntry> le1(new ReplayEntry((char*)log_1, (uint32_t)lsize, (uint64_t)log_seq));
+    proxy.write(log_seq, log_file, log_off, le1);
+
+    log_seq++;
+    log_off += lsize;
+    off_len_t i2 = {30, 10};
+    lsize = sizeof(log_header_t) + sizeof(off_len_t) + i2.length;
+    log_header_t* log_2 = (log_header_t*)malloc(lsize);
+    log_2->count = 1;
+    memcpy((char*)log_2->off_len, &i2, sizeof(off_len_t));
+    char* d2 = (char*)malloc(i2.length);
+    memcpy((char*)log_2+sizeof(log_header_t) + sizeof(off_len_t), d2, i2.length);
+    //shared_ptr<log_header_t> le2(log_2, log_header_deleter());
+    shared_ptr<ReplayEntry> le2(new ReplayEntry((char*)log_2, (uint32_t)lsize, (uint64_t)log_seq));
+    proxy.write(log_seq, log_file, log_off, le2);
+
+    log_seq++;
+    log_off += lsize;
+    off_len_t i3 = {15, 10};
+    lsize = sizeof(log_header_t) + sizeof(off_len_t) + i3.length;
+    log_header_t* log_3 = (log_header_t*)malloc(lsize);
+    log_3->count = 1;
+    memcpy((char*)log_3->off_len, &i3, sizeof(off_len_t));
+    char* d3 = (char*)malloc(i3.length);
+    memcpy((char*)log_3+sizeof(log_header_t) + sizeof(off_len_t), d3, i3.length);
+    //shared_ptr<log_header_t> le3(log_3, log_header_deleter());
+    shared_ptr<ReplayEntry> le3(new ReplayEntry((char*)log_3, (uint32_t)lsize, (uint64_t)log_seq));
+    proxy.write(log_seq, log_file, log_off, le3);
+
+    log_seq++;
+    log_off += lsize;
+    off_len_t i4 = {5, 15};
+    lsize = sizeof(log_header_t) + sizeof(off_len_t) + i4.length;
+    log_header_t* log_4 = (log_header_t*)malloc(lsize);
+    log_4->count = 1;
+    memcpy((char*)log_4->off_len, &i4, sizeof(off_len_t));
+    char* d4 = (char*)malloc(i4.length);
+    memcpy((char*)log_4+sizeof(log_header_t) + sizeof(off_len_t), d4, i4.length);
+    //shared_ptr<log_header_t> le4(log_4, log_header_deleter());
+    shared_ptr<ReplayEntry> le4(new ReplayEntry((char*)log_4, (uint32_t)lsize, (uint64_t)log_seq));
+    proxy.write(log_seq, log_file, log_off, le4);
+   
+    //cout << "test get" << endl;
+    //Bcache* bcache = proxy.get_bcache();
+    //Bkey k(10, 10, 0);
+    //shared_ptr<CEntry> v;
+    //bcache->get(k, v);
+
+    //off_t  r_o = 0;
+    //size_t r_l = 45;
+    //char*  r_b = (char*)malloc(r_l);
+    //proxy.read(r_o, r_l, r_b);
+
+    proxy.trace();
+
+    Jkey top = proxy.top();
+    shared_ptr<CEntry> entry = proxy.retrieve(top);
+    
+    proxy.reclaim(entry);
+
+    proxy.trace();
+
+    pause();
+
+    return 0;
+}
