b2dbd464fe28cbd7e3871c1ccca5e33ce291a753
dsnlevi <hnlgwujing@sina.com>
2016-08-09 22:16:27 -0400

    Implement cache module
    1.move maybe multiplex code from cache recover to common
    2.add mmap file read methold
    
    Change-Id: I059c4e5e21a9f991872d85ad4d8d1d51aa2e5c04

---
+++ src/journal/cache/cache_proxy.h
@@ -0,0 +1,70 @@
+#ifndef _PROXY_H
+#define _PROXY_H
+
+#include <unistd.h>
+#include <string>
+#include <memory>
+#include "common.h"
+#include "jcache.h"
+#include "bcache.h"
+#include "../seq_generator.hpp"
+
+using namespace std;
+
+class CacheProxy
+{
+    const static int MAX_CACHE_LIMIT = (100 * 1024 * 1024); //100MBytes
+public:
+    CacheProxy(){}
+    explicit CacheProxy(string blk_dev){
+        blkdev = blk_dev;
+        jcache = new Jcache(blk_dev);
+        bcache = new Bcache(blk_dev, MAX_CACHE_LIMIT);
+    }
+    
+    /*each volume own a cproxy, disable copy constructor and operator=*/
+    CacheProxy(const CacheProxy& other) = delete;
+    CacheProxy(CacheProxy&& other) = delete;
+    CacheProxy& operator=(const CacheProxy& other) = delete;
+    CacheProxy& operator=(CacheProxy&& other) = delete;
+
+    ~CacheProxy(){
+        delete bcache;
+        delete jcache;
+    }
+    
+    /*journal writer or replayer add cache*/
+    void write(IoVersion log_seq,          //log sequence 
+               string    log_file,         //log file name 
+               off_t     log_off,          //log offset in log file  
+               shared_ptr<ReplayEntry> log_entry);  //log entry
+
+    /*io hook read handle*/
+    int  read(off_t  off,   //offset in original block device
+              size_t len,   //read data size 
+              char*  buf);  //read data will store in buf
+    
+    /*journal replayer call*/
+    /*replayer get log cursor*/
+    Jkey top();                                     
+    /*replayer get CEntry from jcache*/
+    shared_ptr<CEntry> retrieve(const Jkey& key);   
+    /*replayer reclaim CEntry from jcache and bcache */
+    bool reclaim(shared_ptr<CEntry> data);         
+  
+    /*debug*/
+    Bcache* get_bcache()const{
+        return bcache;
+    }
+    Jcache* get_jcache()const{
+        return jcache; 
+    }
+    void trace();
+
+private:
+    string   blkdev;
+    Jcache*  jcache; //accelerate journal replay
+    Bcache*  bcache; //io read cache
+};
+
+#endif
---
+++ src/journal/cache/common.h
@@ -0,0 +1,150 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+
+#include <unistd.h>
+#include <string>
+#include <memory>
+#include "../message.hpp"
+#include "../seq_generator.hpp"
+#include <boost/thread/shared_mutex.hpp>
+
+using namespace std;
+using namespace Journal;
+
+/*boost read write lock */
+typedef boost::shared_mutex       Mutex;
+typedef boost::unique_lock<Mutex> WriteLock;
+typedef boost::shared_lock<Mutex> ReadLock;
+
+/*cache item */
+class CEntry
+{
+    const static uint8_t IN_MEM = 0;
+    const static uint8_t IN_LOG = 1;
+public:
+    CEntry(){}
+    explicit CEntry(IoVersion seq, string file, off_t offset);
+    explicit CEntry(IoVersion seq, string file, off_t offset, 
+                    shared_ptr<ReplayEntry> entry);
+    CEntry(const CEntry& other);
+    CEntry(CEntry&& other);
+    CEntry& operator=(const CEntry& other);
+    CEntry& operator=(CEntry&& other);
+    ~CEntry(){}
+
+    const IoVersion get_log_seq()const{
+        return log_seq;
+    }
+
+    const string get_log_file()const{
+        return log_file;
+    }
+
+    const off_t get_log_offset()const{
+        return log_offset;
+    }
+
+    const uint8_t get_cache_type()const{
+        return cache_type;
+    }
+    const shared_ptr<ReplayEntry> get_log_entry()const{
+        return log_entry;
+    }
+    
+    /*the centry take how many memory space*/
+    size_t get_mem_size()const;
+
+private:
+    IoVersion log_seq;       //io log sequence number
+    string    log_file;      //log file name  
+    off_t     log_offset;    //log entry append to log offset 
+
+    int      cache_type;    //cache in memory or on log file 
+    shared_ptr<ReplayEntry> log_entry; //log entry in memory
+
+    friend class Bcache; 
+    friend class Jcache;
+};
+
+/*interface to genernate hash code when route*/
+class IHashcode
+{
+public:
+    IHashcode() = default; 
+    virtual ~IHashcode(){}
+    virtual int hashcode() = 0;
+};
+
+/*interface to read file*/
+class IReadFile : public IHashcode
+{
+public:
+    static int fid;
+public:
+    IReadFile() = default;
+    IReadFile(string file, off_t pos, bool eos);
+    virtual ~IReadFile(){}
+
+    virtual int open() = 0;
+    virtual void close() = 0;
+    virtual size_t read(off_t off, char* buf, size_t count) = 0;
+
+public:
+    string m_file;  /*file name*/ 
+    int    m_fd;    /*file descriptor*/
+    size_t m_size;  /*file size*/
+    off_t  m_pos;   /*valid data offset*/
+    bool   m_eos;   /*end of stream, true: can not get journal file from drserver*/
+};
+
+/*use synchronize read or pread */
+class SyncReadFile: public IReadFile
+{
+public:
+    SyncReadFile(string file, off_t pos, bool eos);
+    virtual ~SyncReadFile();
+   
+    virtual int open() override;
+    virtual void close() override;
+    virtual size_t read(off_t off, char* buf, size_t count) override;
+    virtual int hashcode() override;
+};
+
+/*use mmap read*/
+class MmapReadFile: public IReadFile
+{
+public:
+    MmapReadFile(string file, off_t pos, bool eos);
+    virtual ~MmapReadFile();
+
+    virtual int open() override;
+    virtual void close() override;
+    virtual size_t read(off_t off, char* buf, size_t count) override;
+    virtual int hashcode() override;
+private:
+    void* m_mmap_base{nullptr};
+}; 
+
+/*interface use to route or dispatch*/
+class IRoute
+{
+public:
+    IRoute()= default;
+    virtual ~IRoute(){} 
+
+    virtual uint32_t route(int hashcode, uint32_t partion_num) = 0;
+};
+
+/*simple hash route implementation*/
+class HashRoute: public IRoute
+{
+public:
+    HashRoute() = default;
+    virtual ~HashRoute(){}
+
+    virtual uint32_t route(int hashcode, uint32_t partion_num){
+        return hashcode % partion_num;
+    }
+};
+
+#endif
