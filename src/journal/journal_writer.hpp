3c78b336eb479df0db2ff2c6567868ce8b97abc7
liuanwang <wangliuan2016@163.com>
2016-08-18 16:06:51 +0800

    Implement journal writer class
    Change-Id: I413afadf911b735756ed8c73139eae539e11a4f3

--- src/journal/journal_writer.hpp
+++ src/journal/journal_writer.hpp
@@ -1,13 +1,69 @@
 #ifndef JOURNAL_WRITER_HPP
 #define JOURNAL_WRITER_HPP
 
+#include <cstdint>
+#include <cstdio>
+#include <mutex>
+#include <condition_variable>
+#include <time.h>
+
+#include <sys/stat.h>
+
+#include <boost/noncopyable.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include "message.hpp"
+#include "replay_entry.hpp"
+#include "../rpc/clients/writer_client.hpp"
+
 namespace Journal{
 
 class JournalWriter
+    :private boost::noncopyable
 {
-    //todo
+public:
+    explicit JournalWriter(std::string rpc_addr,
+                                 entry_queue& write_queue,
+                                 boost::asio::ip::tcp::socket& raw_socket,
+                                 std::condition_variable& cv);
+    virtual ~JournalWriter();
+    void work();
+    bool init(std::string& vol);
+    bool deinit();
+    //The following two function must be called in another thread,can't call in write thread
+    //The write thread and another thread are single consumer/single producer module,communicate with lockfree queue
+    bool get_writeable_journals(const std::string& uuid,const int limit);
+    bool seal_journals(const std::string& uuid);
+private:
+    bool open_journal(uint64_t entry_size);
+    bool get_journal();
+    int64_t get_file_size(const char *path);
+    bool write_journal_header();
+
+    std::mutex mtx_;
+    std::condition_variable& cv_;
+    WriterClient rpc_client;
+    boost::shared_ptr<boost::thread> thread_ptr;
+    entry_queue& write_queue_;
+    boost::asio::ip::tcp::socket& raw_socket_;
+    boost::lockfree::queue<std::string*> journal_queue;
+    boost::lockfree::queue<std::string*> seal_queue;
+
+    FILE* cur_file_ptr;
+    std::string *cur_journal;
+    uint64_t cur_journal_size;
+    uint64_t journal_max_size;
+    
+    std::string vol_id;
+    uint64_t write_seq;
+    double write_timeout;
+    int32_t version;
+    checksum_type_t checksum_type;
 };
 
 }
 
 #endif
+
+
--- src/journal/volume_manager.cpp
+++ src/journal/volume_manager.cpp
@@ -2,6 +2,7 @@
 #include <boost/bind.hpp>
 #include <algorithm>
 #include "connection.hpp"
+#include "../log/log.h"
 
 namespace Journal{
 
@@ -9,29 +10,16 @@ Volume::Volume(boost::asio::io_service& io_service)
     :write_queue_(),
      entry_queue_(),
      raw_socket_(io_service),
-     handler(raw_socket_,write_queue_,entry_queue_),
-     connection(raw_socket_,entry_queue_),
-     writer()
+     pre_processor(raw_socket_,write_queue_,entry_queue_,entry_cv,write_cv),
+     connection(raw_socket_,entry_queue_,entry_cv),
+     writer("localhost:50051",write_queue_,raw_socket_,write_cv)
 {
-    //todo read thread_num from config file
-    int thread_num = 1;
-    buffer_pool = nedalloc::nedcreatepool(BUFFER_POOL_SIZE,thread_num+2);
-    if (buffer_pool == NULL)
-    {
-        //todo use LOG
-        std::cout << "create buffer pool failed" << std::endl;
-    }
-    connection.init(buffer_pool);
-    bool ret = handler.init(buffer_pool,thread_num);
-    if(!ret)
-    {
-        std::cout << "init request handler failed" << std::endl;
-    }
 }
 
 Volume::~Volume()
 {
-    handler.deinit();
+    writer.deinit();
+    pre_processor.deinit();
     connection.deinit();
     
     if (buffer_pool != NULL)
@@ -39,6 +27,34 @@ Volume::~Volume()
         nedalloc::neddestroypool(buffer_pool);
         buffer_pool = NULL;
     }
+}
+bool Volume::init(std::string& vol)
+{
+    //todo read thread_num from config file
+    vol_id = vol;
+    int thread_num = 1;
+    buffer_pool = nedalloc::nedcreatepool(BUFFER_POOL_SIZE,thread_num+2);
+    if(buffer_pool == NULL)
+    {
+        LOG_ERROR << "create buffer pool failed";
+        return false;
+    }
+    if(!connection.init(buffer_pool))
+    {
+        LOG_ERROR << "init connection failed,vol_id:" << vol;
+        return false;
+    }
+    if(!pre_processor.init(buffer_pool,thread_num))
+    {
+        LOG_ERROR << "init pre_processor failed,vol_id:"<< vol;
+        return false;
+    }
+    if(!writer.init(vol))
+    {
+        LOG_ERROR << "init journal writer failed,vol_id:" << vol;
+        return false;
+    }
+    return true;
 }
 
 raw_socket& Volume::get_raw_socket()
@@ -63,14 +79,57 @@ void Volume::stop()
 
 
 //VolumeManager
+VolumeManager::VolumeManager()
+{
+}
+
+VolumeManager::~VolumeManager()
+{
+    thread_ptr->interrupt();
+    thread_ptr->join();
+}
+
+bool VolumeManager::init()
+{
+    thread_ptr.reset(new boost::thread(boost::bind(&VolumeManager::periodic_task, this)));
+}
+
+void VolumeManager::periodic_task()
+{
+    //todo read config.ini
+    int_least64_t interval = 500;
+    int journal_limit = 4;
+    while(true)
+    {
+        boost::this_thread::sleep_for(boost::chrono::milliseconds(interval));
+        std::unique_lock<std::mutex> lk(mtx);
+        for(std::map<std::string,volume_ptr>::iterator iter = volumes.begin();iter!=volumes.end();++iter)
+        {
+            std::string vol_id = iter->first;
+            volume_ptr vol = iter->second;
+            JournalWriter& writer = vol->get_writer();
+            if(!writer.get_writeable_journals("test-uuid",journal_limit))
+            {
+                LOG_ERROR << "get_writeable_journals failed,vol_id:" << vol_id;
+            }
+            if(!writer.seal_journals("test-uuid"))
+            {
+                LOG_ERROR << "seal_journals failed,vol_id:" << vol_id;
+            }
+        }
+    }
+}
+    
 void VolumeManager::start(std::string vol_id,volume_ptr vol)
 {
+    std::unique_lock<std::mutex> lk(mtx);
     volumes.insert(std::pair<std::string,volume_ptr>(vol_id,vol));
     vol->start();
 }
 
 void VolumeManager::stop(std::string vol_id)
 {
+    std::unique_lock<std::mutex> lk(mtx);
     std::map<std::string,volume_ptr>::iterator iter;
     iter = volumes.find(vol_id);
     if (iter != volumes.end())
@@ -83,6 +142,7 @@ void VolumeManager::stop(std::string vol_id)
 
 void VolumeManager::stop_all()
 {
+    std::unique_lock<std::mutex> lk(mtx);
     for(std::map<std::string,volume_ptr>::iterator iter = volumes.begin();iter!=volumes.end();++iter)
     {
         volume_ptr vol = iter->second;
