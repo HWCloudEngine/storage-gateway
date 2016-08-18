f9cef1ea42430b02e6c94494b3b354457ba4c1ef
liuanwang <wangliuan2016@163.com>
2016-08-18 16:06:51 +0800

    Implement journal writer class
    Change-Id: I413afadf911b735756ed8c73139eae539e11a4f3

--- src/journal/volume_manager.cpp
+++ src/journal/volume_manager.cpp
@@ -2,6 +2,7 @@
 #include <boost/bind.hpp>
 #include <algorithm>
 #include "connection.hpp"
+#include "../log/log.h"
 
 namespace Journal{
 
@@ -9,29 +10,16 @@ Volume::Volume(boost::asio::io_service& io_service)
     :write_queue_(),
      entry_queue_(),
      raw_socket_(io_service),
-     handler(raw_socket_,write_queue_,entry_queue_),
-     connection(raw_socket_,entry_queue_),
-     writer()
+     pre_processor(raw_socket_,write_queue_,entry_queue_,entry_cv,write_cv),
+     connection(raw_socket_,entry_queue_,entry_cv),
+     writer("localhost:50051",write_queue_,raw_socket_,write_cv)
 {
-    //todo read thread_num from config file
-    int thread_num = 1;
-    buffer_pool = nedalloc::nedcreatepool(BUFFER_POOL_SIZE,thread_num+2);
-    if (buffer_pool == NULL)
-    {
-        //todo use LOG
-        std::cout << "create buffer pool failed" << std::endl;
-    }
-    connection.init(buffer_pool);
-    bool ret = handler.init(buffer_pool,thread_num);
-    if(!ret)
-    {
-        std::cout << "init request handler failed" << std::endl;
-    }
 }
 
 Volume::~Volume()
 {
-    handler.deinit();
+    writer.deinit();
+    pre_processor.deinit();
     connection.deinit();
     
     if (buffer_pool != NULL)
@@ -39,6 +27,39 @@ Volume::~Volume()
         nedalloc::neddestroypool(buffer_pool);
         buffer_pool = NULL;
     }
+}
+bool Volume::init()
+{
+    //todo read thread_num from config file
+    int thread_num = 1;
+    buffer_pool = nedalloc::nedcreatepool(BUFFER_POOL_SIZE,thread_num+2);
+    if(buffer_pool == NULL)
+    {
+        LOG_ERROR << "create buffer pool failed";
+        return false;
+    }
+    if(!connection.init(buffer_pool))
+    {
+        LOG_ERROR << "init connection failed,vol_id:" << vol_id_;
+        return false;
+    }
+    if(!pre_processor.init(buffer_pool,thread_num))
+    {
+        LOG_ERROR << "init pre_processor failed,vol_id:"<< vol_id_;
+        return false;
+    }
+    if(!writer.init(vol_id_))
+    {
+        LOG_ERROR << "init journal writer failed,vol_id:" << vol_id_;
+        return false;
+    }
+    return true;
+}
+
+void Volume::set_property(std::string vol_id,std::string vol_path)
+{
+    vol_id_ = vol_id;
+    vol_path_ = vol_path;
 }
 
 raw_socket& Volume::get_raw_socket()
@@ -63,14 +84,109 @@ void Volume::stop()
 
 
 //VolumeManager
-void VolumeManager::start(std::string vol_id,volume_ptr vol)
+VolumeManager::VolumeManager()
 {
-    volumes.insert(std::pair<std::string,volume_ptr>(vol_id,vol));
-    vol->start();
+}
+
+VolumeManager::~VolumeManager()
+{
+    thread_ptr->interrupt();
+    thread_ptr->join();
+}
+
+bool VolumeManager::init()
+{
+    thread_ptr.reset(new boost::thread(boost::bind(&VolumeManager::periodic_task, this)));
+}
+
+void VolumeManager::periodic_task()
+{
+    //todo read config.ini
+    int_least64_t interval = 500;
+    int journal_limit = 4;
+    while(true)
+    {
+        boost::this_thread::sleep_for(boost::chrono::milliseconds(interval));
+        std::unique_lock<std::mutex> lk(mtx);
+        for(std::map<std::string,volume_ptr>::iterator iter = volumes.begin();iter!=volumes.end();++iter)
+        {
+            std::string vol_id = iter->first;
+            volume_ptr vol = iter->second;
+            JournalWriter& writer = vol->get_writer();
+            if(!writer.get_writeable_journals("test-uuid",journal_limit))
+            {
+                LOG_ERROR << "get_writeable_journals failed,vol_id:" << vol_id;
+            }
+            if(!writer.seal_journals("test-uuid"))
+            {
+                LOG_ERROR << "seal_journals failed,vol_id:" << vol_id;
+            }
+        }
+    }
+}
+
+void VolumeManager::add_vol(volume_ptr vol)
+{
+    boost::asio::async_read(vol->get_raw_socket(),
+    boost::asio::buffer(header_buffer_, sizeof(struct IOHookRequest)),
+    boost::bind(&VolumeManager::handle_request_header, this,vol,
+                 boost::asio::placeholders::error));
+}
+
+void VolumeManager::handle_request_header(volume_ptr vol,const boost::system::error_code& e)
+{
+    IOHookRequest* header_ptr = reinterpret_cast<IOHookRequest *>(header_buffer_.data());
+    if(!e && header_ptr->magic == MESSAGE_MAGIC )
+    {
+        if(header_ptr->type == ADD_VOLUME)
+        {
+                boost::asio::async_read(vol->get_raw_socket(),
+                boost::asio::buffer(body_buffer_, sizeof(struct add_vol_req)),
+                boost::bind(&VolumeManager::handle_request_body, this,vol,
+                boost::asio::placeholders::error));
+        }
+        else
+        {
+            //todo
+            std::cerr << "first message is not ADD_VOLUME";
+        }
+    }
+    else
+    {
+        //todo bad request
+        ;
+    }
+
+}
+
+void VolumeManager::handle_request_body(volume_ptr vol,const boost::system::error_code& e)
+{
+    if(!e)
+    {
+        add_vol_req_t* header_ptr = reinterpret_cast<add_vol_req_t *>(body_buffer_.data());
+        std::string vol_id = std::string(header_ptr->volume_name);
+        std::string vol_path = std::string(header_ptr->device_path);
+        std::unique_lock<std::mutex> lk(mtx);
+        vol->set_property(vol_id,vol_path);
+        volumes.insert(std::pair<std::string,volume_ptr>(vol_id,vol));
+        vol->init();
+        vol->start();
+    }
+    else
+    {
+        //todo
+        ;
+    }
+}
+    
+void VolumeManager::start(volume_ptr vol)
+{
+    add_vol(vol);
 }
 
 void VolumeManager::stop(std::string vol_id)
 {
+    std::unique_lock<std::mutex> lk(mtx);
     std::map<std::string,volume_ptr>::iterator iter;
     iter = volumes.find(vol_id);
     if (iter != volumes.end())
@@ -83,6 +199,7 @@ void VolumeManager::stop(std::string vol_id)
 
 void VolumeManager::stop_all()
 {
+    std::unique_lock<std::mutex> lk(mtx);
     for(std::map<std::string,volume_ptr>::iterator iter = volumes.begin();iter!=volumes.end();++iter)
     {
         volume_ptr vol = iter->second;
--- src/journal/volume_manager.hpp
+++ src/journal/volume_manager.hpp
@@ -2,14 +2,18 @@
 #define JOURNAL_VOLUME_MANAGER_HPP
 
 #include <set>
+#include <mutex>
+#include <condition_variable>
 #include <boost/asio.hpp>
 #include <boost/noncopyable.hpp>
+#include <boost/thread/thread.hpp>
 #include "connection.hpp"
 #include "journal_writer.hpp"
-#include "request_handler.hpp"
+#include "pre_processor.hpp"
 #include "nedmalloc.h"
 
-#define BUFFER_POOL_SIZE 1024*1024*32
+#define BUFFER_POOL_SIZE 1024*1024*64
+#define REQUEST_BODY_SIZE 512
 
 namespace Journal{
 
@@ -24,28 +28,52 @@ public:
     raw_socket& get_raw_socket();
     void start();
     void stop();
+    bool init();
+    void set_property(std::string vol_id,std::string vol_path);
 private:
+    void periodic_task();
     entry_queue write_queue_;
     entry_queue entry_queue_;
     raw_socket raw_socket_;
+
+    std::condition_variable entry_cv;
+    std::condition_variable write_cv;
         
-    RequestHandler handler;
+    PreProcessor pre_processor;
     Connection connection;
     JournalWriter writer;
 
     nedalloc::nedpool * buffer_pool;
+
+    std::string vol_id_;
+    std::string vol_path_;
 };
 typedef boost::shared_ptr<Volume> volume_ptr;
+
 
 class VolumeManager
     :private boost::noncopyable
 {
 public:
-    void start(std::string vol_id,volume_ptr vol);
+    VolumeManager();
+    virtual ~VolumeManager();
+    void start(volume_ptr vol);
     void stop(std::string vol_id);
     void stop_all();
+    bool init();
+    void add_vol(volume_ptr vol);
 private:
+    void periodic_task();
+    void handle_request_header(volume_ptr vol,const boost::system::error_code& e);
+    void handle_request_body(volume_ptr vol,const boost::system::error_code& e);
+    std::mutex mtx;
+    boost::shared_ptr<boost::thread> thread_ptr;
     std::map<std::string,volume_ptr> volumes;
+    boost::array<char, HEADER_SIZE> header_buffer_;
+    boost::array<char, 512> body_buffer_;
+
+    int_least64_t interval;
+    int journal_limit;
 };
 }
 
