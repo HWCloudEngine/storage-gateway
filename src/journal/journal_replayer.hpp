5832858daf6afddac8f6e9f7e16ae39e5f182dfb
smile-luobin <luobin_smile@163.com>
2016-08-27 07:49:40 +0000

    Implement journal replayer
    Change-Id: Ifb7baee83bd28568efed5739823a9da62fd991d0

--- src/journal/journal_replayer.hpp
+++ src/journal/journal_replayer.hpp
@@ -12,32 +12,45 @@
 #include <atomic>
 #include <thread>
 #include "../rpc/clients/replayer_client.hpp"
+#include "./cache_recover/cache_recover.hpp"
 
 namespace Journal {
 
 class JournalReplayer {
     //todo
 public:
-    JournalReplayer(const std::string& rpc_addr, const std::string& vol_id);
+    JournalReplayer(const std::string& rpc_addr, const std::string& vol_id,
+            const std::string& device,
+            std::shared_ptr<CacheProxy> cache_proxy_ptr);
+    void start_recover();
+    void stop_recover();
+
     void start_replay(int interval_time);
     void stop_replay();
-    void init_cache();
 private:
-    void replay(int interval_time);
+    void replay_thread(int interval_time);
     void replay_volume();
-    void init_cache_thread();
+    bool replay_cache();
+    bool replay_journal_file();
+    bool process_file(const std::string& journal);
 
     bool get_journal_marker();
     bool get_journal_list(int limit);
     bool update_consumer_marker();
 
     std::string vol_id;
+    std::string device;
+    int replay_count;
     std::atomic_bool stop_atomic;
-    std::shared_ptr<std::thread> replay_thread_ptr;
 
-    ReplayerClient rpc_client;
     JournalMarker journal_marker;
     std::list<std::string> journals;
+
+    std::unique_ptr<std::thread> replay_thread_ptr;
+    std::shared_ptr<ReplayerClient> rpc_client_ptr;
+    std::unique_ptr<CacheRecovery> cache_recover_ptr;
+    std::shared_ptr<CacheProxy> cache_proxy_ptr;
+    std::shared_ptr<IDGenerator> id_maker_ptr;
 };
 
 }
--- src/journal/replay_manager.hpp
+++ src/journal/replay_manager.hpp
@@ -12,23 +12,28 @@
 #include <mutex>
 #include <string>
 #include "journal_replayer.hpp"
+#include "./cache/cache_proxy.h"
 
 namespace Journal {
 
 class ReplayManager {
 public:
     ReplayManager(const std::string& rpc_addr, int interval_time);
-    bool add_vol_replayer(const std::string& vol_id);
+    bool add_vol_replayer(const std::string& vol_id, const std::string& device);
     bool remove_vol_replayer(const std::string& vol_id);
+    std::shared_ptr<CacheProxy> get_cache_proxy_ptr(const std::string& vol_id);
 private:
     void start_replay();
-    void init_cache();
+    void recover_cache();
     void init();
 
     int interval_time;
     std::string rpc_addr;
     std::mutex map_mutex;
-    std::map<std::string, std::shared_ptr<JournalReplayer>> vol_replayer_map;
+
+    std::map<std::string, std::shared_ptr<CacheProxy>> vol_proxy_map;
+    std::map<std::string, std::string> vol_device_map;
+    std::map<std::string, std::unique_ptr<JournalReplayer>> vol_replayer_map;
 };
 
 }
