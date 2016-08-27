8895c8f400f51e5f8cae9c4c3fd2aeccfea8ebe1
smile-luobin <luobin_smile@163.com>
2016-08-27 07:49:40 +0000

    Implement journal replayer
    Change-Id: Ifb7baee83bd28568efed5739823a9da62fd991d0

--- src/journal/replay_manager.cpp
+++ src/journal/replay_manager.cpp
@@ -9,40 +9,79 @@
 
 namespace Journal {
 
-ReplayManager::ReplayManager(const std::string& rpc_addr, int interval_time) :
-        interval_time(interval_time), rpc_addr(rpc_addr) {
-    init();
-    init_cache();
+ReplayManager::ReplayManager(int replay_interval,
+        int update_interval,
+        const std::string& rpc_addr,
+        std::vector<std::string> vols,
+        std::vector<std::string> devices,
+        std::shared_ptr<IDGenerator> id_maker_ptr) :
+            replay_interval(replay_interval),
+            update_interval(update_interval),
+            rpc_addr(rpc_addr),
+            id_maker_ptr(id_maker_ptr){
+
+    //init vol_*_map
+    int vol_num = vols.size();
+    for(int index = 0; index < vol_num; index++){
+        std::string vol_id = vols[index];
+        std::string device = devices[index];
+        std::shared_ptr<CacheProxy> cache_proxy_ptr(new CacheProxy(vol_id));
+        std::unique_ptr<JournalReplayer> journal_replayer_ptr(
+                new JournalReplayer(replay_interval, update_interval,
+                        rpc_addr, vol_id, device,
+                        cache_proxy_ptr, id_maker_ptr));
+
+        vol_device_map[vol_id] = device;
+        vol_proxy_map[vol_id] = cache_proxy_ptr;
+        vol_replayer_map[vol_id] = journal_replayer_ptr;
+    }
+
+    //recover replay cache
+    recover_cache();
+
+    //start replay
     start_replay();
 }
 
-void ReplayManager::init() {
-    //todo: init vol_replayer_map from conf
-}
 
-void ReplayManager::init_cache() {
+//recover cache
+void ReplayManager::recover_cache() {
+    //start recover cache
     for (auto vol_replayer_pair : vol_replayer_map) {
-        std::shared_ptr<JournalReplayer> journal_replayer_ptr(
-                new JournalReplayer(rpc_addr, vol_replayer_pair.first));
-        vol_replayer_pair.second = journal_replayer_ptr;
-        journal_replayer_ptr->init_cache();
+        vol_replayer_pair.second->start_recover();
+    }
+
+    //stop recover cache
+    for (auto vol_replayer_pair : vol_replayer_map) {
+        vol_replayer_pair.second->stop_recover();
     }
 }
 
+//replay journal
 void ReplayManager::start_replay() {
     for (auto vol_replayer_pair : vol_replayer_map) {
-        journal_replayer_ptr->start_replay(interval_time);
+        journal_replayer_ptr->start_replay();
     }
 }
 
-bool ReplayManager::add_vol_replayer(const std::string& vol_id) {
+bool ReplayManager::add_vol_replayer(const std::string& vol_id,
+        const std::string& device) {
     std::lock_guard<std::mutex> lg(map_mutex);
     auto pos = vol_replayer_map.find(vol_id);
     if (pos == vol_replayer_map.end()) {
-        std::shared_ptr<JournalReplayer> journal_replayer_ptr(
-                new JournalReplayer(rpc_addr, pos->first));
+        //init cache proxy
+        std::shared_ptr<CacheProxy> cache_proxy_ptr(new CacheProxy(vol_id));
+        //init journal replayer
+        std::unique_ptr<JournalReplayer> journal_replayer_ptr(
+                new JournalReplayer(replay_interval, update_interval,
+                        rpc_addr, vol_id, device, cache_proxy_ptr,
+                        id_maker_ptr));
         vol_replayer_map[vol_id] = journal_replayer_ptr;
-        journal_replayer_ptr->start_replay(vol_id, interval_time);
+        vol_proxy_map[vol_id] = cache_proxy_ptr;
+        vol_device_map[vol_id] = deivce;
+
+        //start replay
+        journal_replayer_ptr->start_replay();
         return true;
     } else {
         return false;
@@ -54,10 +93,23 @@ bool ReplayManager::remove_vol_replayer(const std::string& vol_id) {
     auto pos = vol_replayer_map.find(vol_id);
     if (pos != vol_replayer_map.end()) {
         pos->second->stop_replay();
-        vol_replayer_map.erase(pos);
+        vol_replayer_map.erase(vol_id);
+        vol_device_map.erase(vol_id);
+        vol_proxy_map.erase(vol_id);
         return true;
     } else {
         return false;
+    }
+}
+
+//get cache proxy
+std::shared_ptr<CacheProxy> ReplayManager::get_cache_proxy_ptr(
+        const std::string& vol_id) {
+    auto pos = vol_proxy_map.find(vol_id);
+    if (pos != vol_proxy_map.end()) {
+        return vol_proxy_map[vol_id];
+    } else {
+        return nullptr;
     }
 }
 
--- src/journal/replay_manager.hpp
+++ src/journal/replay_manager.hpp
@@ -11,24 +11,36 @@
 #include <map>
 #include <mutex>
 #include <string>
+#include <vector>
 #include "journal_replayer.hpp"
+#include "./cache/cache_proxy.h"
 
 namespace Journal {
 
 class ReplayManager {
 public:
-    ReplayManager(const std::string& rpc_addr, int interval_time);
-    bool add_vol_replayer(const std::string& vol_id);
+    ReplayManager(int replay_interval,
+            int update_interval,
+            const std::string& rpc_addr,
+            std::vector<std::string> vols,
+            std::vector<std::string> devices,
+            std::shared_ptr<IDGenerator> id_maker_ptr);
+    bool add_vol_replayer(const std::string& vol_id, const std::string& device);
     bool remove_vol_replayer(const std::string& vol_id);
+    std::shared_ptr<CacheProxy> get_cache_proxy_ptr(const std::string& vol_id);
 private:
     void start_replay();
-    void init_cache();
-    void init();
+    void recover_cache();
 
-    int interval_time;
+    int replay_interval;  //replay interval
+    int update_interval;
     std::string rpc_addr;
     std::mutex map_mutex;
-    std::map<std::string, std::shared_ptr<JournalReplayer>> vol_replayer_map;
+
+    std::shared_ptr<IDGenerator> id_maker_ptr;
+    std::map<std::string, std::shared_ptr<CacheProxy>> vol_proxy_map;
+    std::map<std::string, std::string> vol_device_map;
+    std::map<std::string, std::unique_ptr<JournalReplayer>> vol_replayer_map;
 };
 
 }
