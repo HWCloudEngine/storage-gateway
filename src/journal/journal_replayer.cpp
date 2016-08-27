13768d2fa041d6251a064e88a57f22c397df4020
smile-luobin <luobin_smile@163.com>
2016-08-27 07:49:40 +0000

    Implement journal replayer
    Change-Id: Ifb7baee83bd28568efed5739823a9da62fd991d0

--- src/journal/journal_replayer.cpp
+++ src/journal/journal_replayer.cpp
@@ -5,37 +5,47 @@
  *      Author: smile-luobin
  */
 
+#include <cstdio>
 #include <iostream>
 #include "journal_replayer.hpp"
 
 namespace Journal {
 
 JournalReplayer::JournalReplayer(const std::string& rpc_addr,
-        const std::string& vol_id) :
-        vol_id(vol_id),
-        rpc_client(grpc::CreateChannel(rpc_addr,
-                grpc::InsecureChannelCredentials())),
-        journal_marker() {
+        const std::string& vol_id, const std::string& device,
+        std::shared_ptr<CacheProxy> cache_proxy_ptr) :
+        vol_id(vol_id), device(device), journal_marker(),
+        cache_proxy_ptr(cache_proxy_ptr) {
+    rpc_client_ptr.reset(
+            new ReplayerClient(
+                    grpc::CreateChannel(rpc_addr,
+                            grpc::InsecureChannelCredentials())));
+    id_maker_ptr.reset(new IDGenerator());
     stop_atomic = false;
 }
 
-void JournalReplayer::replay(int interval_time) {
-    while (!stop_atomic.load()) {
-        replay_volume();
-        std::this_thread::sleep_for(std::chrono::seconds(interval_time));
+//start recover cache
+void JournalReplayer::start_recover() {
+    cache_recover_ptr.reset(
+            new CacheRecovery(vol_id, rpc_client_ptr, id_maker_ptr));
+    cache_recover_ptr->start();
+}
+
+//stop recover cache
+void JournalReplayer::stop_recover() {
+    if (cache_recover_ptr.get()) {
+        cache_recover_ptr->stop();
     }
 }
 
-void JournalReplayer::replay_volume() {
-    //todo: replay volume
-}
-
+//start replay volume
 void JournalReplayer::start_replay(int interval_time) {
     replay_thread_ptr.reset(
-            new std::thread(&JournalReplayer::replay, this, vol_id,
+            new std::thread(&JournalReplayer::replay_thread, this, vol_id,
                     interval_time));
 }
 
+//stop replay volume
 void JournalReplayer::stop_replay() {
     if (replay_thread_ptr.get()) {
         stop_atomic.store(true);
@@ -44,35 +54,157 @@ void JournalReplayer::stop_replay() {
     }
 }
 
-void JournalReplayer::init_cache() {
-    auto init_thread = std::thread(&JournalReplayer::init_cache_thread, this, vol_id);
-    init_thread.join();
+void JournalReplayer::replay_thread(int interval_time) {
+    while (!stop_atomic.load()) {
+        replay_volume();
+        std::this_thread::sleep_for(std::chrono::seconds(interval_time));
+    }
 }
 
-void JournalReplayer::init_cache_thread() {
-    //todo: init volume cache
+//replay volume
+void JournalReplayer::replay_volume() {
+    //todo: config replay_number(current only replay one journal)
+    if (false == replay_cache()) {
+        replay_journal_file();
+    }
+}
+
+//replay from cache
+bool JournalReplayer::replay_cache() {
+    //todo: config replay_number(current only replay one journal)
+    Jkey top = cache_proxy_ptr->top();
+    if (nullptr != top) {
+        std::shared_ptr<CEntry> entry = cache_proxy_ptr->retrieve(top);
+        if (entry->cache_type == entry->IN_MEM) {
+            //todo: replay from memory
+            FILE* vol_file_ptr = fopen(device.c_str(), "ab+");
+            fwrite(entry->log_entry->data(), entry->log_offset,
+                    entry->log_entry->length(), vol_file_ptr);
+            fclose(vol_file_ptr);
+            vol_file_ptr = nullptr;
+        } else {
+            if (false == process_file(entry->get_log_file())) {
+                return false;
+            }
+        }
+        cache_proxy_ptr->reclaim(entry);
+
+        //update consumer marker
+        journal_marker.set_cur_journal(entry->get_log_file());
+        journal_marker.clear_pos();
+        update_consumer_marker();
+
+        std::cout << "replay from cache succeed" << std::endl;
+        return true;
+    } else {
+        std::cout << "no jcache" << std::endl;
+        return false;
+    }
+}
+
+//replay from journal file
+bool JournalReplayer::replay_journal_file() {
+    if (nullptr != vol_file_ptr) {
+        //todo:config limit in conf
+        //todo: config replay_number(current only replay one journal)
+        int limit = 1;
+        if (get_journal_marker()) {
+            if (get_journal_list(1)) {
+                for (auto journal : journals) {
+                    if (false == process_file(journal)) {
+                        return false;
+                    }
+                }
+                //update journal marker
+                if (journals.size() < limit) {
+                    journal_marker.clear_cur_journal();
+                    journal_marker.clear_pos();
+                } else {
+                    journal_marker.set_cur_journal(*journals.rbegin());
+                    journal_marker.clear_pos();
+                }
+                update_consumer_marker();
+            }
+        }
+    }
+    return true;
+}
+
+bool JournalReplayer::process_file(const std::string& file_name) {
+    FILE* vol_file_ptr = fopen(device.c_str(), "ab+");
+    if (nullptr != vol_file_ptr) {
+        FILE* src_file_ptr = fopen(file_name.c_str(), "r");
+        if (nullptr != src_file_ptr) {
+            /*read header*/
+            log_header_t log_head;
+            memset(&log_head, 0, sizeof(log_head));
+            size_t head_size = sizeof(log_head);
+            int ret = fread((void*) &log_head, sizeof(char), head_size,
+                    src_file_ptr);
+            if (ret != sizeof(log_head)) {
+                std::cout << "read log head failed ret=" << ret << std::endl;
+                return false;
+            }
+
+            /*read off len */
+            size_t off_len_size = log_head.count * sizeof(off_len_t);
+            off_len_t* off_len = (off_len_t*) malloc(off_len_size);
+            ret = fread((void*) off_len, sizeof(char), off_len_size,
+                    src_file_ptr);
+            if (ret != off_len_size) {
+                std::cout << "read log off len failed ret=" << ret << std::endl;
+                return false;
+            }
+
+            /*read data*/
+            for (int i = 0; i < log_head.count; i++) {
+                uint64_t off = log_head.off_len[i].offset;
+                uint32_t length = log_head.off_len[i].length;
+                char* data = (char*) malloc(sizeof(char) * length);
+                ret = fread((void*) data, sizeof(char), length, src_file_ptr);
+                if (ret != length) {
+                    std::cout << "read data failed ret=" << ret << std::endl;
+                    return false;
+                }
+                ret = fwrite(data, off, length, vol_file_ptr);
+                if (ret != length) {
+                    std::cout << "replay data failed ret=" << ret << std::endl;
+                    return false;
+                }
+            }
+            std::cout << "replay from journal file succeed" << std::endl;
+            return true;
+        } else {
+            std::cout << "open journal file failed" << std::endl;
+            return false;
+        }
+    } else {
+        std::cout << "open volume file failed" << std::endl;
+        return false;
+    }
 }
 
 bool JournalReplayer::get_journal_marker() {
-    if(rpc_client.GetJournalMarker(vol_id, journal_marker)){
+    if (rpc_client_ptr->GetJournalMarker(vol_id, journal_marker)) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
 bool JournalReplayer::get_journal_list(int limit) {
-    if(rpc_client.GetJournalList(vol_id, journal_marker, limit, journals)){
+    if (rpc_client_ptr->GetJournalList(vol_id, journal_marker, limit,
+            journals)) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
-bool JournalReplayer::update_consumer_marker(){
-    if(rpc_client.UpdateConsumerMarker(journal_marker, vol_id)){
+bool JournalReplayer::update_consumer_marker() {
+    if (rpc_client_ptr->UpdateConsumerMarker(journal_marker, vol_id)) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
--- src/journal/replay_manager.cpp
+++ src/journal/replay_manager.cpp
@@ -12,36 +12,62 @@ namespace Journal {
 ReplayManager::ReplayManager(const std::string& rpc_addr, int interval_time) :
         interval_time(interval_time), rpc_addr(rpc_addr) {
     init();
-    init_cache();
+    recover_cache();
     start_replay();
 }
 
 void ReplayManager::init() {
-    //todo: init vol_replayer_map from conf
+    //todo: init vol_device_map and vol_replayer_map from conf
 }
 
-void ReplayManager::init_cache() {
+//recover cache
+void ReplayManager::recover_cache() {
     for (auto vol_replayer_pair : vol_replayer_map) {
-        std::shared_ptr<JournalReplayer> journal_replayer_ptr(
-                new JournalReplayer(rpc_addr, vol_replayer_pair.first));
+        std::string vol_id = vol_replayer_pair.first;
+
+        //init cache proxy
+        std::shared_ptr<CacheProxy> cache_proxy_ptr(new CacheProxy(vol_id));
+        vol_proxy_map[vol_id] = cache_proxy_ptr;
+
+        //init journal_replayer
+        std::unique_ptr<JournalReplayer> journal_replayer_ptr(
+                new JournalReplayer(rpc_addr, vol_replayer_pair.first,
+                        vol_device_map[vol_id]), cache_proxy_ptr);
         vol_replayer_pair.second = journal_replayer_ptr;
-        journal_replayer_ptr->init_cache();
+
+        //start recover
+        journal_replayer_ptr->recover_cache();
+    }
+    for(auto vol_replayer_pair: vol_replayer_map){
+        //stop recover
+        vol_replayer_pair.second->stop_recover();
     }
 }
 
+//replay journal
 void ReplayManager::start_replay() {
     for (auto vol_replayer_pair : vol_replayer_map) {
         journal_replayer_ptr->start_replay(interval_time);
     }
 }
 
-bool ReplayManager::add_vol_replayer(const std::string& vol_id) {
+bool ReplayManager::add_vol_replayer(const std::string& vol_id,
+        const std::string& device) {
     std::lock_guard<std::mutex> lg(map_mutex);
     auto pos = vol_replayer_map.find(vol_id);
     if (pos == vol_replayer_map.end()) {
-        std::shared_ptr<JournalReplayer> journal_replayer_ptr(
-                new JournalReplayer(rpc_addr, pos->first));
+        //init cache proxy
+        std::shared_ptr<CacheProxy> cache_proxy_ptr(new CacheProxy(vol_id));
+        vol_proxy_map[vol_id] = cache_proxy_ptr;
+
+        //init journal replayer
+        std::unique_ptr<JournalReplayer> journal_replayer_ptr(
+                new JournalReplayer(rpc_addr, vol_id, device));
         vol_replayer_map[vol_id] = journal_replayer_ptr;
+
+        vol_device_map[vol_id] = deivce;
+
+        //start replay
         journal_replayer_ptr->start_replay(vol_id, interval_time);
         return true;
     } else {
@@ -55,9 +81,22 @@ bool ReplayManager::remove_vol_replayer(const std::string& vol_id) {
     if (pos != vol_replayer_map.end()) {
         pos->second->stop_replay();
         vol_replayer_map.erase(pos);
+        vol_device_map.erase(vol_id);
+        vol_proxy_map.erase(vol_id);
         return true;
     } else {
         return false;
+    }
+}
+
+//get cache proxy
+std::shared_ptr<CacheProxy> ReplayManager::get_cache_proxy_ptr(
+        const std::string& vol_id) {
+    auto pos = vol_proxy_map.find(vol_id);
+    if (pos != vol_proxy_map.end()) {
+        return vol_proxy_map[vol_id];
+    } else {
+        return nullptr;
     }
 }
 
