1d98b56efb725eed8eb9d261efd82d10887fa579
huangguoqiang <hgqislub@qq.com>
2017-03-21 20:39:21 +0800

    implement tooz C API
    1/ use uds implement tooz callback API
    2/ start heartbeat thread
    3/ start run_watchers thread
    4/ start callback listen thread
    TODO:
    log,make.am,callback message queue,integrate with sgserver
    
    Change-Id: I27748fb9790ca3f0abddcea7704e1df968f32fa3

---
+++ src/tooz_client/tooz_client.h
@@ -0,0 +1,43 @@
+/*************************************************************************
+    > File Name: sg-tooz.h
+    > Author: h00277488
+    > Mail: huangguoqiang2@huawei.com 
+    > Created Time: Wed 15 Mar 2017 04:42:23 PM CST
+ ************************************************************************/
+#include <thread>
+#include <string>
+#include <mutex>
+#include <Python.h>
+#include "tooz_api.h"
+#define SOCKET_FILE     "./sg_uds"
+#define BUF_MAX_LEN     4096
+using namespace std;
+class ToozClient
+{
+public:
+    ToozClient();
+    ~ToozClient();
+    void start_coordination(string backend_url, string member_id);
+    void join_group(string group_id);
+    void leave_group(string group_id);
+    void watch_group(string group_id);
+    void stop_coordination();
+private:
+    int terminate_heartbeat = 1;
+    int terminate_run_watchers = 1;
+    PyObject *pInstance = NULL; 
+    thread *hb_t;
+    thread *watch_t;
+    thread *callback_t;
+    void get_env();
+    void start_callback_server_thread();
+    void start_heartbeat_thread();
+    void start_run_watchers_thread();
+    void stop_callback_thread();
+    void stop_run_watchers_thread();
+    void stop_heartbeat_thread();
+    void heartbeat();
+    void callback_server();
+    int callback(int connection_fd);
+    void run_watchers();
+};
