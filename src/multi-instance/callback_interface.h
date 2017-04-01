e08375555c1cb6340f5e0624e9df66151706255e
huangguoqiang <hgqislub@qq.com>
2017-04-01 15:36:15 +0800

    tooz client API
    1/ create/watch/join/leave group
    2/ register callback when group changed
    3/ hashring rebalance
    
    Change-Id: I00e5f55cad51d518783c82431939af9dc3b333a8

---
+++ src/multi-instance/callback_interface.h
@@ -0,0 +1,16 @@
+#ifndef _ICALLBACK_H
+#define _ICALLBACK_H
+#include <string>
+#include <list>
+using namespace std;
+
+class ICallback
+{
+public:
+    ICallback(){}
+    virtual ~ICallback(){}
+    virtual void join_group_callback(const list<string> &old_buckets, const list<string> &new_buckets) = 0;
+    virtual void leave_group_callback(const list<string> &old_buckets, const list<string> &new_buckets) = 0;
+};
+
+#endif
