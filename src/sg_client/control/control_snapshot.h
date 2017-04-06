36fcc1094ec784ec16250e486af1c15b64b422ad
dsnlevi <wujing7@huawei.com>
2017-04-06 21:42:10 +0800

    create volume from snapshot
    1. implement basic logic
    
    Change-Id: I61ee56449f7b9bf0d7c63d7fdc33aa1ac7fcf91a

--- src/sg_client/control/control_snapshot.h
+++ src/sg_client/control/control_snapshot.h
@@ -1,16 +1,16 @@
 #ifndef CONTROL_SNAPSHOT_H_
 #define CONTROL_SNAPSHOT_H_
-
+#include <sys/time.h>
 #include <map>
+#include <deque>
 #include <memory>
 #include <mutex>
 #include <thread>
 #include <functional>
 #include <grpc++/grpc++.h>
-#include "../../rpc/common.pb.h"
-#include "../../rpc/snapshot_control.pb.h"
-#include "../../rpc/snapshot_control.grpc.pb.h"
-#include "../../log/log.h"
+#include "rpc/common.pb.h"
+#include "rpc/snapshot_control.pb.h"
+#include "rpc/snapshot_control.grpc.pb.h"
 #include "../snapshot/snapshot_proxy.h"
 #include "../volume.h"
 
@@ -32,6 +32,10 @@ using huawei::proto::control::DiffSnapshotReq;
 using huawei::proto::control::DiffSnapshotAck;
 using huawei::proto::control::ReadSnapshotReq;
 using huawei::proto::control::ReadSnapshotAck;
+using huawei::proto::control::CreateVolumeFromSnapReq;
+using huawei::proto::control::CreateVolumeFromSnapAck;
+using huawei::proto::control::QueryVolumeFromSnapReq;
+using huawei::proto::control::QueryVolumeFromSnapAck;
 
 using namespace std;
 using namespace Journal;
@@ -40,12 +44,8 @@ using namespace Journal;
 class SnapshotControlImpl final: public SnapshotControl::Service 
 { 
 public:
-    SnapshotControlImpl(map<string, shared_ptr<Volume>>& volumes)
-        :m_volumes(volumes){
-    }
-
-    virtual ~SnapshotControlImpl(){
-    }
+    SnapshotControlImpl(map<string, shared_ptr<Volume>>& volumes);
+    virtual ~SnapshotControlImpl();
 
     Status CreateSnapshot(ServerContext* context, 
                           const CreateSnapshotReq* req, 
@@ -75,12 +75,46 @@ public:
                         const ReadSnapshotReq* req, 
                         ReadSnapshotAck* ack) override;
 
+    Status CreateVolumeFromSnap(ServerContext* context, 
+                                const CreateVolumeFromSnapReq* req, 
+                                CreateVolumeFromSnapAck* ack) override;
+
+    Status QueryVolumeFromSnap(ServerContext* context, 
+                               const QueryVolumeFromSnapReq* req, 
+                               QueryVolumeFromSnapAck* ack) override;
+
 private:
     /*each volume own corresponding snapshot proxy*/
     shared_ptr<SnapshotProxy> get_vol_snap_proxy(const string& vol_name);
+    
+    void bg_work();
+    void bg_reclaim();
 
 private:
     map<string, shared_ptr<Volume>>& m_volumes;
+
+    /*create volume from snapshot*/
+    const static int BG_INIT  = 0;
+    const static int BG_DOING = 1;
+    const static int BG_DONE  = 2;
+    const static int BG_ERR   = 3;
+    struct BgJob
+    {
+        string new_volume;
+        string new_blk_device;
+        string vol_name;
+        string snap_name;
+        int    status;
+        struct timeval start_ts;
+        struct timeval complete_ts;
+        struct timeval expire_ts;
+    };
+
+    BlockingQueue<struct BgJob*>* m_pending_queue;
+    deque<struct BgJob*>* m_complete_queue;
+    atomic_bool m_run;
+    thread* m_work_thread;
+    thread* m_reclaim_thread;
 };
 
 #endif
