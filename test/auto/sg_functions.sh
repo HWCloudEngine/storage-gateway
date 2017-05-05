d793233b15f9ea7cc297f9eeb55fbc965b35aad0
toddyLee <layu_lee@yahoo.com>
2017-05-06 01:16:03 +0800

    add auto test framework
    usage:
    1. write test with a name "tNNNN-xxx-xx.sh", where NNNN should be numbers,
       and the first charactor behind NNNN should be '-';
    2. the tests will be run in sequence, so please notify the dependence
    3. run `make` in the auto dirctory, all the test will be run in order,
       it will stop if any test failed
    4. the logs was collected to /var/log/storage-gateway(maybe on other jenkins hosts)
    
    Change-Id: I836c193ff79fd0098e16d5fc5c42004e6b2cb195

---
+++ test/auto/sg_functions.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+SG_LOG_HOME=/var/log/storage-gateway
+
+sg_print()
+{
+#    printf "$@" >> ${SG_LOG_HOME}/$(date +"%Y_%m_%d").log
+#    printf "\n" >> ${SG_LOG_HOME}/$(date +"%Y_%m_%d").log
+     echo -n "`date` :" >> ${SG_LOG_HOME}/$(date +"%Y_%m_%d").log
+     echo "$@" >> ${SG_LOG_HOME}/$(date +"%Y_%m_%d").log
+     sync
+}
+
+# check if B is substr of A
+# usage: is_substr B A
+is_substr()
+{
+    success=$(echo "$2" | grep -E "$1")
+    if [[ $success == "" ]]; then
+        return 1
+    fi
+    return 0
+}
