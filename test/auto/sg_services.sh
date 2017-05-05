7c20566c28b62ea79ae5970443cdc016ce76c78c
toddyLee <layu_lee@yahoo.com>
2017-05-06 01:16:03 +0800

    add auto test framework
    usage:
    1. write test with a name "tNNNN-xxx-xx.sh", where NNNN should be numbers,
       and the first charactor behind NNNN should be '-';
    2. the tests will be run in sequence, so please notify the dependence
    3. run `make` in the auto dirctory, all the test will be run in order,
       it will stop if any test failed
    4. the logs was collected to /var/log/storage-gateway(maybe on other jenkins hosts)
    
    Change-Id: I836c193ff79fd0098e16d5fc5c42004e6b2cb195

---
+++ test/auto/sg_services.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# start or stop sg services
+
+source ./sg_env.sh
+. ${SG_SCRIPTS_HOME}/sg_functions.sh
+
+if [ $2 ]
+then
+  TEST_VOLUME_ID=$2
+fi
+
+case $1 in
+    'start')
+        sg_print "do start"
+        export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+        sg_server_pid=`pgrep sg_server`
+        sg_client_pid=`pgrep sg_client`
+        tgt_status=`/etc/init.d/tgt status`
+        if [ "$sg_server_pid" = "" ]; then
+            cd ${SG_SOURCE_HOME}/src/sg_server
+            ulimit -c unlimited
+            nohup ./sg_server &
+        fi
+
+        if [ "$sg_client_pid" = "" ]; then
+            cd ${SG_SOURCE_HOME}/src/sg_client
+            ulimit -c unlimited
+            nohup ./sg_client ${CTRL_SERVER_HOST} ${CTRL_SERVER_PORT} ${UNIX_DOMAIN_PATH} &
+            sleep 2 
+        fi
+
+        if [ "$tgt_status" = "tgt stop/waiting" ]; then
+            nohup /etc/init.d/tgt start &
+            sleep 1
+        fi
+
+        #check & verify process exsit
+        sg_print "verify all services are running"
+        sg_server_pid=`pgrep sg_server`
+        sg_client_pid=`pgrep sg_client`
+        tgt_status=`/etc/init.d/tgt status`
+        if [[ "$sg_server_pid" = ""  || 
+            "$sg_client_pid" = "" || 
+            "$tgt_status" = "tgt stop/waiting" ]]; then
+            sg_print "start sg services failed!"
+            exit 1
+        fi
+        sg_print "start sg done!"
+        exit 0
+        ;;
+
+    'stop')
+        sg_print "do stop "
+        /etc/init.d/tgt stop
+        pgrep -f tgtd | xargs kill -9
+        pgrep -f sg_client | xargs kill -9
+        pgrep -f sg_server | xargs kill -9
+        ;;
+
+    'clean')
+        sg_print "do clean `date` "
+        rm /var/log/storage-gateway/* -f
+        rm ${SG_SOURCE_HOME}/sg_client/sg_client.log* -f
+        rm ${SG_SOURCE_HOME}/sg_server/sg_server.log* -f
+        rm ${SG_SOURCE_HOME}/sg_client/core -f
+        rm ${SG_SOURCE_HOME}/sg_server/core -f
+        if [ ! -z ${TEST_VOLUME_ID} ]
+        then
+          rm /mnt/cephfs/journals/${TEST_VOLUME_ID}/* -rf
+          rm /var/tmp/${TEST_VOLUME_ID}/ -rf
+          rm /etc/tgt/conf.d/${TEST_VOLUME_ID} -f
+        fi
+        ;;
+    *)
+        sg_print "$0 $1 not support"
+        exit 1
+        ;;
+esac
---
+++ test/auto/sg_test.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+# test between two hosts 
+
+source ./sg_env.sh
+. ${SG_SCRIPTS_HOME}/sg_functions.sh
+
+operate_uuid=`cat /proc/sys/kernel/random/uuid`
+primary_role="primary"
+secondary_role="secondary"
+
+cd ${SG_SCRIPTS_HOME}
+
+case $1 in
+    'start')
+        sg_print "start sg process"
+        ssh ${LOCAL_HOST} "cd ${SG_SCRIPTS_HOME}; ./sg_services.sh start < /dev/null > /dev/null 2>&1"
+        res1=$?
+        ssh ${REMOTE_HOST} "cd ${SG_SCRIPTS_HOME}; ./sg_services.sh start </dev/null > /dev/null 2>&1"
+        res2=$?
+        ;;
+    'stop')
+        sg_print "stop sg process"
+        ssh ${LOCAL_HOST} "cd ${SG_SCRIPTS_HOME}; ./sg_services.sh stop < /dev/null > /dev/null 2>&1"
+        res1=$?
+        ssh ${REMOTE_HOST} "cd ${SG_SCRIPTS_HOME}; ./sg_services.sh stop < /dev/null > /dev/null 2>&1"
+        res2=$?
+        ;;
+    'clean')
+        sg_print "clean test records"
+        ssh  ${LOCAL_HOST} "cd ${SG_SCRIPTS_HOME}; ./sg_services.sh stop; sleep 3; ./sg_services.sh clean ${PRIMARY_VOLUME}"
+        res1=$?
+        ssh  ${REMOTE_HOST} "cd ${SG_SCRIPTS_HOME}; ./sg_services.sh stop; sleep 3; ./sg_services.sh clean ${SECONDARY_VOLUME}"
+        res2=$?
+        ;;
+
+    'enable')
+        sg_print "enable volume "
+        ssh  ${LOCAL_HOST} "cd ${SG_SCRIPTS_HOME};./volume.sh enable ${PRIMARY_VOLUME} < /dev/null > /dev/null 2>&1"
+        res1=$?
+        ssh  ${REMOTE_HOST} "cd ${SG_SCRIPTS_HOME}; ./volume.sh enable ${SECONDARY_VOLUME} < /dev/null > /dev/null 2>&1"
+        res2=$?
+        ;;
+
+    'disable')
+        sg_print "disable volume "
+        ssh  ${LOCAL_HOST} "cd ${SG_SCRIPTS_HOME};./volume.sh disable ${PRIMARY_VOLUME} < /dev/null > /dev/null 2>&1"
+        res1=$?
+        ssh  ${REMOTE_HOST} "cd ${SG_SCRIPTS_HOME}; ./volume.sh disable ${SECONDARY_VOLUME} < /dev/null > /dev/null 2>&1"
+        res2=$?
+        ;;
+
+    'create_rep')
+        sg_print "create_rep"
+        ssh  ${LOCAL_HOST} "cd ${SG_SCRIPTS_HOME};./replicate.sh create_rep ${operate_uuid} ${primary_role} ${PRIMARY_VOLUME} ${SECONDARY_VOLUME}"
+        res1=$?
+        # reverse PRIMARY_VOLUME and SECONDARY_VOLUME for remote site
+        ssh  ${REMOTE_HOST} "cd ${SG_SCRIPTS_HOME}; ./replicate.sh create_rep ${operate_uuid} ${secondary_role} ${SECONDARY_VOLUME} ${PRIMARY_VOLUME} "
+        res2=$?
+        ;;
+    'enable_rep')
+        sg_print "enable replication"
+        ssh  ${LOCAL_HOST} "cd ${SG_SCRIPTS_HOME};./replicate.sh enable_rep ${operate_uuid} ${primary_role} ${PRIMARY_VOLUME}"
+        res1=$?
+        ssh  ${REMOTE_HOST} "cd ${SG_SCRIPTS_HOME}; ./replicate.sh enable_rep ${operate_uuid} ${secondary_role} ${SECONDARY_VOLUME}"
+        res2=$?
+        ;;
+    'disable_rep')
+        sg_print "disable replication"
+        ssh  ${REMOTE_HOST} "cd ${SG_SCRIPTS_HOME}; ./replicate.sh disable_rep ${operate_uuid} ${secondary_role} ${SECONDARY_VOLUME}"
+        res1=$?
+        sg_print ""
+        ssh  ${LOCAL_HOST} "cd ${SG_SCRIPTS_HOME};./replicate.sh disable_rep ${operate_uuid} ${primary_role} ${PRIMARY_VOLUME}"
+        res2=$?
+        ;;
+    'failover_rep')
+        sg_print "failover replication remote"
+        ssh  ${REMOTE_HOST} "cd ${SG_SCRIPTS_HOME}; ./replicate.sh failover_rep ${operate_uuid} ${secondary_role} ${SECONDARY_VOLUME}"
+        res1=$?
+        sg_print "failover replication local"
+        ssh  ${LOCAL_HOST} "cd ${SG_SCRIPTS_HOME};./replicate.sh failover_rep ${operate_uuid} ${primary_role} ${PRIMARY_VOLUME}"
+        res2=$?
+        ;;
+    'delete_rep')
+        sg_print "delete replication"
+        ssh  ${REMOTE_HOST} "cd ${SG_SCRIPTS_HOME}; ./replicate.sh delete_rep ${operate_uuid} ${secondary_role} ${SECONDARY_VOLUME}"
+        res1=$?
+        sg_print ""
+        ssh  ${LOCAL_HOST} "cd ${SG_SCRIPTS_HOME};./replicate.sh delete_rep ${operate_uuid} ${primary_role} ${PRIMARY_VOLUME}"
+        res2=$?
+        ;;
+    'reverse_rep')
+        sg_print "reverse replication"
+        ssh  ${REMOTE_HOST} "cd ${SG_SCRIPTS_HOME}; ./replicate.sh reverse_rep ${operate_uuid} ${secondary_role} ${SECONDARY_VOLUME}"
+        res1=$?
+        sg_print ""
+        ssh  ${LOCAL_HOST} "cd ${SG_SCRIPTS_HOME};./replicate.sh reverse_rep ${operate_uuid} ${primary_role} ${PRIMARY_VOLUME}"
+        res2=$?
+        ;;
+    *)
+        sg_print "$0 $1 not support"
+        exit 1
+        ;;
+esac
+    if [[ $res1 -ne 0 || $res2 -ne 0 ]]  ; then
+        sg_print "operation $1 failed!"
+        sg_print "operation $1 result: $res1 $res2"
+        exit 1
+    else
+        sg_print "operation $1 ok!"
+    fi
